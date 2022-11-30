d965cdd1091a36f840fd35649291cc2e437a373f
tony bui <buiminhtien06@gmail.com>
2022-11-30 17:55:32 +1100

    aa

--- auto/insertPropertyId.js
+++ auto/insertPropertyId.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { updateFile, processArgs } = require("./update-file-helper");
-
+const github = require('@actions/github');
 const stringsToReplace = ["// Your custom aliases here - do not delete"];
 const args = processArgs();
 
 const updates = (fileContent) => {
   console.log(args, 'a22');
-  const { propertyId } = args;
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  const propertyId = github.context.payload.client_payload.propertyId
+  // const { propertyId } = args;
 
   return fileContent.replace(
     stringsToReplace[0],
